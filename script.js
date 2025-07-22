fdd9c96b8612da9fc0df2ad280d69def1539778c
japavell <laicans512@gmail.com>
2025-07-22 02:34:57 -0300

    Add files via upload
    Verçao primaria

---
+++ script.js
@@ -0,0 +1,12 @@
+function canalizar() {
+    alert("Você canalizou sorte! 🍀 Uma boa oportunidade se aproxima.");
+}
+
+function meditar() {
+    alert("Você meditou sobre o lucro. 🧘 Paz e sabedoria estão com você.");
+}
+
+function arriscar() {
+    const resultado = Math.random() > 0.5 ? "ganhou energia!" : "perdeu energia...";
+    alert("Você arriscou e " + resultado);
+}
